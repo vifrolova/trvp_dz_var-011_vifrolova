e0bd64e1e7004e3b3eeb879941c2853a8b639e23
vifrolova <nikabel30@yandex.ru>
2025-06-23 15:24:37 +0300

    Update new.c 2

--- new.c
+++ new.c
@@ -1,32 +1,6 @@
-#include <stdio.h>
+#include "ext_modules.h"
 
-#define VOID const int
-
-void print2(const int a) {
-	printf("%d", a);
-}
-
-int foo( void) 
+int ext_modules( void)
 {
 	return 0;
-}
-
-int main() {
-    int num1, num2, sum;
-
-    // Запрос и ввод первого числа
-    printf("Введите первое число: ");
-    scanf("%d", &num1);
-
-    // Запрос и ввод второго числа
-    printf("Введите второе число: ");
-    scanf("%d", &num2);
-
-    // Вычисление суммы
-    sum = num1 + num2;
-
-    // Вывод результата
-    printf("Сумма чисел %d и %d равна %d\n", num1, num2, sum);
-
-    return 0;
 }
